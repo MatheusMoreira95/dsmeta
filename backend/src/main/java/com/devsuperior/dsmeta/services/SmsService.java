fe8eb5aaa9c8bf7806b9f654472d224ae57fc63a
Matheus Moreira <matheusjosemoreira@gmail.com>
2022-09-11 00:51:20 -0300

    Send Notification

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -37,8 +37,11 @@ public class SmsService {
 		String date =sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
 		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+			    + " com um total de R$ " + String.format("%.0f", sale.getAmount());
+		
+		/*String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
 			    + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
-
+*/
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
